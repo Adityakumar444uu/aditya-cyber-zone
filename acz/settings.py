82c914c70883c04b4db5c2148bd8463cfb0dbd43
Aditya Kumar <ak9598168@gmail.com>
2026-05-18 18:06:18 +0530

    add postgres

--- acz/settings.py
+++ acz/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,12 +52,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'acz.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
