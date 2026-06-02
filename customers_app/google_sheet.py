ac65a727d4f7688ea09a768ef4bda63d413bd943
Aditya Kumar <ak9598168@gmail.com>
2026-06-02 18:32:12 +0530

    Enable Google Sheet sync

--- customers_app/google_sheet.py
+++ customers_app/google_sheet.py
@@ -1,2 +1,37 @@
+import os
+import gspread
+from django.conf import settings
+from google.oauth2.service_account import Credentials
+
+
 def sync_application_to_sheet(application, remark=""):
-    return
+    try:
+        scopes = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        creds_path = os.path.join(settings.BASE_DIR, "credentials.json")
+
+        creds = Credentials.from_service_account_file(creds_path, scopes=scopes)
+        client = gspread.authorize(creds)
+
+        sheet = client.open_by_key("1OYOj6T4d24dfAdF86Z9OGxxJWru_oRBB33X0oTQiVYI").sheet1
+        customer = application.customer
+
+        sheet.append_row([
+            customer.name,
+            customer.aadhaar_no,
+            customer.contact_no,
+            application.application_name,
+            application.application_no,
+            str(application.application_date),
+            application.status,
+            str(application.delivery_date) if application.delivery_date else "",
+            remark,
+        ])
+
+        print("Google Sheet Sync Success")
+
+    except Exception as e:
+        print("Google Sheet Sync Error:", e)
