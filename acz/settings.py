09f20fc2cc33a4635dd4acd2492ad4a79f75cf57
Aditya Kumar <ak9598168@gmail.com>
2026-07-02 12:45:27 +0530

    Fix Razorpay settings

--- acz/settings.py
+++ acz/settings.py
@@ -118,6 +118,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
-RAZORPAY_KEY_SECRET = os.environ.get("FLIFqomvrbakoDjyGbC8T0N3")
+RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID", "rzp_live_T8JDbEb05oO6nK")
+RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET", "RYcpUzaX2ITf4dSiP2DBFOg5")
 RAZORPAY_WEBHOOK_SECRET = os.environ.get("RAZORPAY_WEBHOOK_SECRET", "")
